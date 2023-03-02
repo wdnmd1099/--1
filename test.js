746fa381a25d825e31b283d8d2af8d853684b67a
Kasan <879611700@qq.com>
2023-03-03 01:05:00 +0800

    更新注释

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
 import {eventHub} from './xxx' 
 
-// test.js 订阅了一个名为click的事件
-eventHub.on('click', (data)=>{console.log("test.js函数被触发" + data)})
+const fn1 = (data)=>{
+    console.log("test.js函数被触发" + data)
+}
 
+// test.js 订阅了一个名为click的事件
+eventHub.on('click', fn1)
+eventHub.off('click', fn1)
 setTimeout(() => {  
-    // 在三秒后发布此事件，所有订阅了click事件的订阅者将会执行订阅时所传的函数
+    // 在一秒后发布此事件，所有订阅了click事件的订阅者
+    //将会接收到来自emit传的data（'kasan'）作为形参并传给订阅时所传的函数（fn1）
     eventHub.emit('click', 'kasan')
-}, 3000);
+}, 1000);
 
 console.log(eventHub)
--- xxx.js
+++ xxx.js
@@ -1,25 +1,28 @@
 // 这个xxx.js 是订阅发布的逻辑，导出此逻辑给其它模块引用
 export const eventHub = {
+    // map 是消息队列，存储所有的订阅者，发布时，先订阅的先执行
     map: {
         // click: [f1 , f2 , ...]
         // click1:[f1 , f2 , ...]
     },
-    on: (name, fn) => {
+    on: (name, fn) => { // name 是监听的事件名， fn 是监听的事件被发布后触发的函数，on是指订阅
         //如果没有此事件就新建一个空数组以存储新事件
         eventHub.map[name] = eventHub.map[name] || []
         eventHub.map[name].push(fn)
     },
-    emit: (name, data) => {
+    emit: (name, data) => { // name 是需要发布的事件名，
+        //data 会传给所有订阅了此事件的订阅者，订阅了此事件的订阅者会接收data并传给 on 的 fn 函数
         const q = eventHub.map[name]
-        if (!q) return
+        if (!q) {console.error('不存在此事件'); return}
         q.map(f => f.call(null, data))
         return undefined
     },
-    off: (name, fn) => {
+    off: (name, fn) => { // 取消订阅
         const q = eventHub.map[name]
         if (!q) { return }
         const index = q.indexOf(fn)
-        if (index < 0) { return }
+        if (index < 0) {  console.error('取消订阅失败'); return }
         q.splice(index, 1)
+        console.log('取消订阅成功')
     }
 }
